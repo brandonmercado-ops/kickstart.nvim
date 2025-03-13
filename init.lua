f3e15985179d6310ae07203877fc1105df012aa1
Brandon Mercado <brandon.mercado.264@my.csun.edu>
2025-03-13 01:39:20 -0700

    adding nvzone showkeys plugin

--- init.lua
+++ init.lua
@@ -270,6 +270,17 @@ require('lazy').setup({
     },
   },
 
+  {
+    'nvzone/showkeys',
+    cmd = 'ShowkeysToggle',
+    opts = {
+      timeout = 1,
+      maxkeys = 3,
+      position = 'bottom-center',
+      -- more opts
+    },
+  },
+
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
