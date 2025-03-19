ac1b61f92c78691105c213d2a63fdc6537a89881
Brandon Mercado <brandon.mercado.264@my.csun.edu>
2025-03-19 12:50:03 -0700

    custom keymap for neotree

--- init.lua
+++ init.lua
@@ -186,8 +186,9 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 -- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 --
 
--- CUSTOM KEYMAP FOR TOGGLETERM PLUGIN
+-- CUSTOM KEYMAP FOR TOGGLETERM PLUGIN AND NEOTREE
 vim.keymap.set('n', '<leader>tt', '<cmd>ToggleTerm<CR>')
+vim.keymap.set('n', '<leader>nt', '<cmd>Neotree<CR>')
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
